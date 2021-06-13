85be41393d0653932a085d280077ffaf01b25657
Naman <namanv2@gmail.com>
2021-06-14 01:36:52 +0530

    Arithematic operations file created and updated

---
+++ src/allfunc.java
@@ -0,0 +1,25 @@
+public class allfunc {
+    float c;
+    
+        public void add(float a,float b) 
+        {
+            c=a+b;
+            System.out.println(c);
+        }
+        public void multiply(float a,float b)
+        {
+            c=a*b;
+            System.out.println(c);
+        } 
+        public void divide(float a,float b) 
+        {
+            c=a/b;
+            System.out.println(c);
+        }
+        public void percentage(float a,float b) 
+        {
+            c=(a*100)/b;
+            
+            System.out.println(c);
+        }
+}
